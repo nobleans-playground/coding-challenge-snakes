ab7c726bd4709019e22e1ff39c2f7c2a6a8f3ab6
Ramon Wijnands <ramon.wijnands@nobleo.nl>
2023-08-24 10:18:27 +0200

    Remove deathmatch style game mode

--- tournament.py
+++ tournament.py
@@ -10,16 +10,9 @@ from snakes.bots import bots
 from snakes.game import Game, RoundType
 
 
-def main(match_type, games):
+def main(games):
     grid_size = (16, 8)
-
-    if match_type == 'round-robin' or match_type == 'rr':
-        round_robin(games, grid_size=grid_size)
-    elif match_type == 'deathmatch' or match_type == 'dm':
-        deathmatch(games, grid_size=grid_size)
-    else:
-        assert False
-    return
+    round_robin(games, grid_size=grid_size)
 
 
 def round_robin(games, grid_size):
@@ -36,26 +29,6 @@ def round_robin(games, grid_size):
                 agents = {a: bots[a](id=a, grid_size=grid_size), b: bots[b](id=b, grid_size=grid_size)}
                 results = single_game(grid_size, agents)
                 writer.writerow(results)
-
-        f.seek(0)
-        df = pandas.read_csv(f)
-        print(df)
-        print(f'\ngame were written to {f.name}')
-
-
-def deathmatch(games, grid_size):
-    # write to a temporary file so that we have partial scores in case of a crash
-    with NamedTemporaryFile('w+', suffix='.csv', delete=False) as f:
-        print(f'writing game results to {f.name}')
-        writer = csv.writer(f)
-        # write bot names
-        writer.writerow(Bot(id=i, grid_size=grid_size).name for i, Bot in enumerate(bots))
-        writer = csv.DictWriter(f, fieldnames=range(len(bots)))
-
-        for _ in range(games):
-            agents = {i: Bot(id=i, grid_size=grid_size) for i, Bot in enumerate(bots)}
-            results = single_game(grid_size, agents)
-            writer.writerow(results)
 
         f.seek(0)
         df = pandas.read_csv(f)
@@ -80,7 +53,6 @@ def single_game(grid_size, agents):
 
 if __name__ == '__main__':
     parser = ArgumentParser(description='Nobleo Snakes')
-    parser.add_argument('match_type', choices=['round-robin', 'rr', 'deathmatch', 'dm'])
     parser.add_argument('-g', '--games', default=10, type=int, help="Number of games to play")
     args = parser.parse_args()
 
