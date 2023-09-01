a32659579913805fc0c408d5ef79f6c7de6fbe82
Ramon Wijnands <ramon.wijnands@nobleo.nl>
2023-09-01 16:13:09 +0200

    Calculate CPU/turn

--- elo.py
+++ elo.py
@@ -16,14 +16,17 @@ def main(infile):
     names = [name for name in df.columns if name != 'turns' and not name.startswith('cpu_')]
     ranking = df[names]
 
-    print(f'{"Name":25} Wins  Rate  Matches CPU')
+    print(f'{"Name":25} Wins   Rate Matches   CPU CPU/t')
     firsts = ranking == 1
     for col in ranking:
         wins = np.count_nonzero(firsts[col])
         matches = np.count_nonzero(np.isfinite(ranking[col]))
         winrate = 100 * wins / matches if matches else float('nan')
         cpu = df['cpu_' + col].sum()
-        print(f'{col:25} {np.count_nonzero(firsts[col]):4} {winrate:5.1f}% {matches:7} {cpu:.3f}')
+        turns = df['turns'][df[col].notna()].sum()
+        cpu_per_turn = cpu / turns
+        print(f'{col:25} {np.count_nonzero(firsts[col]):4} {winrate:5.1f}% {matches:7}',
+              f'{cpu:5.1f} {cpu_per_turn * 1000:5.1f}')
     print()
 
     elos = estimate_elo(ranking)
