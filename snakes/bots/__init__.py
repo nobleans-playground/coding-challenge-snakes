850ae783da9736d15f4cae5608f63358eead35e3
Ferry Schoenmakers <f.b.f.schoenmakers@gmail.com>
2023-09-06 20:53:37 +0200

    Add FurryMuncher bot

--- snakes/bots/__init__.py
+++ snakes/bots/__init__.py
@@ -14,6 +14,7 @@ from .bram.bot import Slytherin
 from .daniel.bot import Explorer
 from .rokusottervanger.bot import OtterByte
 from .mukunda.bot import Snakunamatata
+from .ferry.bot import FurryMuncher
 
 bots = (
     Random,
@@ -28,4 +29,5 @@ bots = (
     Explorer,
     OtterByte,
     Snakunamatata,
+    FurryMuncher,
 )
